42882c3af0b095a41118add6bb3b2bb817837cc1
Brian Muhia <poppingtonic@gmail.com>
2016-04-18 22:45:58 +0300

    Initial commit

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,4 @@
+(ns env.android.main
+  (:require [consilience.android.core :as core]))
+
+(core/init)
---
+++ test/consilience/core_test.clj
@@ -0,0 +1,7 @@
+(ns consilience.core-test
+  (:require [clojure.test :refer :all]
+            [consilience.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
