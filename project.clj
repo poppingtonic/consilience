087e0713c2a90e60beb5871b9844d017bec4354b
Brian Muhia <poppingtonic@gmail.com>
2016-04-18 23:14:47 +0300

    Add description

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject consilience "0.1.0-SNAPSHOT"
-            :description "FIXME: write description"
+            :description "My blog as a progressive web app"
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0"]
                            [org.clojure/clojurescript "1.7.170"]
                            [org.omcljs/om "1.0.0-alpha28" :exclusions [cljsjs/react cljsjs/react-dom]]
-[natal-shell "0.1.6"]]
+                           [natal-shell "0.1.6"]]
             :plugins [[lein-cljsbuild "1.1.1"]
                       [lein-figwheel "0.5.0-6"]]
             :clean-targets ["target/" "index.ios.js" "index.android.js"]
